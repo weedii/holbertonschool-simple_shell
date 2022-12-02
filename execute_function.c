3ef08193b37f767c2e3300cf772c119d31c20efa
weedii <abidiw293@gmail.com>
2022-12-02 22:48:42 +0100

    edbet

--- execute_function.c
+++ execute_function.c
@@ -1,69 +1,79 @@
 #include "shell.h"
 
-char *builtin_str[] = {"exit"};
-int (*builtin_func[])(char **) = {&exit_function};
-
-/***/
+/**
+ * lsh_launch - function that lunch the other executable function
+ * Return: 1
+ */
 
 int lsh_launch(char **args)
 {
-    pid_t pid;
-    int status;
+	pid_t pid;
+	int status;
 
-    pid = fork();
-    if (pid == 0)
-    {
-        /*Child process*/
-        if (execvp(args[0], args) == -1)
-        {
-            perror("lsh");
-        }
-        exit(EXIT_FAILURE);
-    }
-    else if (pid < 0)
-    {
-        /*Error forking*/
-        perror("lsh");
-    }
-    else
-    {
-        /*Parent process*/
-        do
-        {
-            waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
+	pid = fork();
+	if (pid == 0)
+	{
+		/*Child process*/
+		if (execvp(args[0], args) == -1)
+		{
+			perror("lsh");
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		/*Error forking*/
+		perror("lsh");
+	}
+	else
+	{
+		/*Parent process*/
+		do
+		{
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
 
-    return (1);
+	return (1);
 }
 
-/***/
+/**
+ * exit_function - function exit
+ * Return: 0
+ */
 
-int exit_function()
+int exit_function(void)
 {
-    return (0);
+	return (0);
 }
 
-/***/
+/**
+ * execute_function - function that execute function
+ * @args:  pointer to arguments
+ * Return: execute
+ */
 
 int execute_function(char **args)
 {
-    int i, size;
+	char *builtin_str[] = {"exit"};
+	int i, size;
 
-    size = sizeof(builtin_str) / sizeof(char *);
-    if (args[0] == NULL)
-    {
-        /*An empty command was entered*/
-        return (1);
-    }
+	int (*builtin_func[])(char **) = {&exit_function};
 
-    for (i = 0; i < size; i++)
-    {
-        if (strcmp(args[0], builtin_str[i]) == 0)
-        {
-            return (*builtin_func[i])(args);
-        }
-    }
+	size = sizeof(builtin_str) / sizeof(char *);
+	if (args[0] == NULL)
+	{
+		/*An empty command was entered*/
+		return (1);
+	}
 
-    return (lsh_launch(args));
+	for (i = 0; i < size; i++)
+	{
+		if (strcmp(args[0], builtin_str[i]) == 0)
+		{
+			return ((*builtin_func[i])(args));
+		}
+	}
+
+	return (lsh_launch(args));
 }
