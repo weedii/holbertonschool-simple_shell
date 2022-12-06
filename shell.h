9df1cc47f38ebe42c576adb669044bd69136ee24
weedii <abidiw293@gmail.com>
2022-12-06 19:52:05 +0100

    header file

--- shell.h
+++ shell.h
@@ -9,10 +9,13 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+extern char **environ;
+
 char **str_split(char *line, char *delim);
 void free_array(char **array);
 void loop(int input);
 void get_path(char **cmd);
 void exec_command(char **cmd);
+void print_environment(void);
 
 #endif
